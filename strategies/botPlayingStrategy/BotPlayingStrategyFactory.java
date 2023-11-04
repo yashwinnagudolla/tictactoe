6b3b8afc797e1f6eadb5e4f42e2b69c58cfc8031
Yashwin Nagudolla <yashwinnagudolla@gmail.com>
2023-11-03 21:39:54 -0400

    Bot Playing Strategy Implemented

--- strategies/botPlayingStrategy/BotPlayingStrategyFactory.java
+++ strategies/botPlayingStrategy/BotPlayingStrategyFactory.java
@@ -1,4 +1,14 @@
-package strategies.botPlayingStrategy;
+package tictactoe.strategies.botPlayingStrategy;
+
+import strategies.botPlayingStrategy.BotPlayingStrategy;
+import tictactoe.models.BotDifficultyLevel;
 
 public class BotPlayingStrategyFactory {
+    public static BotPlayingStrategy getBotPlayingStrategyForDifficultyLevel(BotPlayingStrategy level){
+        return switch(level){
+            case EASY -> new tictactoe.strategies.botPlayingStrategy.RandomBotPlayingStrategy();
+            case MEDIUM -> new tictactoe.strategies.botPlayingStrategy.RandomBotPlayingStrategy();
+            case HARD -> new tictactoe.strategies.botPlayingStrategy.RandomBotPlayingStrategy();
+        };
+    }
 }
--- strategies/botPlayingStrategy/RandomBotPlayingStrategy.java
+++ strategies/botPlayingStrategy/RandomBotPlayingStrategy.java
@@ -1,4 +1,25 @@
-package strategies.botPlayingStrategy;
+package tictactoe.strategies.botPlayingStrategy;
 
-public class RandomBotPlayingStrategy {
+import strategies.botPlayingStrategy.BotPlayingStrategy;
+import tictacetoe.models.Player;
+import tictactoe.models.Cell;
+import tictactoe.models.CellState;
+import tictactoe.models.Move;
+
+public class RandomBotPlayingStrategy implements BotPlayingStrategy {
+    @Override
+    public Move makeMove(Player player, tictactoe.models.Board board){
+        for(int i = 0;i < board.getSize();i++){
+            for(int j = 0;j < board.getSize();j++){
+                if(board.getBoard().get(i).get(j).getCellState().equals(CellState.EMPTY)){
+                    board.getBoard().get(i).get(j).setPlayer(player);
+                    board.getBoard().get(i).get(j).setCellState(CellState.FILLED);
+                    return new Move(new Cell(i,j), player);
+                }
+
+            }
+        }
+        return null;
+    }
+
 }
